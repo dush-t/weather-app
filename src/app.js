d23325aaff0493ba4ce1a538cc560c4a01b0e726
Dushyant Yadav <dushyant9309@gmail.com>
2019-05-17 11:05:51 +0530

    Setup app for deployment on heroku

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ console.log(__dirname);
 console.log(path.join(__dirname, '../public'));
 
 const app = express();
+const port = process.env.PORT || 3000;  // If no port variable is found in the environment.
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -110,6 +111,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('The server is online on port 3000');
+app.listen(port, () => {
+    console.log('The server is online on port ' + port);
 });
